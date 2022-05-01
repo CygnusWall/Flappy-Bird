04307ec8bfe03b58a9cea43d9f4600692859640b
CygnusWall <96161984+CygnusWall@users.noreply.github.com>
2022-05-01 13:21:32 -0400

    Virtual Resolution Fixed

--- main.lua
+++ main.lua
@@ -1,20 +1,20 @@
 push = require 'push'
 
-WINDOW_WIDTH = 1366
-WINDOW_HEIGHT = 768
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
 
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
 local background = love.graphics.newImage('background.png')
-local background = love.graphics.newImage('ground.png')
+local ground = love.graphics.newImage('ground.png')
 
 function love.load()
 	love.graphics.setDefaultFilter('nearest', 'nearest')
 
 	love.window.setTitle('Flappy Bird')
 
-	push:setupScreen(VIRTUAL_HEIGHT, VIRTUAL_WIDTH, WINDOW_WIDTH, WINDOW_HEIGHT, {
+	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
 		vsync = true,
 		fullscreen = false,
 		resizable = false
@@ -29,4 +29,13 @@ function love.keyPressed(key)
 	if key == 'escape' then
 		love.event.quit()
 	end
+end
+
+function love.draw()
+	push:start()
+
+	love.graphics.draw(background, 0, 0)
+	love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+
+	push:finish()
 end
