1ebf955949281012270476bf91fb9ed86c7fbe97
CygnusWall <96161984+CygnusWall@users.noreply.github.com>
2022-05-01 11:32:50 -0400

    Testing Something

--- main.lua
+++ main.lua
@@ -13,4 +13,20 @@ function love.load()
 	love.graphics.setDefaultFilter('nearest', 'nearest')
 
 	love.window.setTitle('Flappy Bird')
+
+	push:setupScreen(VIRTUAL_HEIGHT, VIRTUAL_WIDTH, WINDOW_WIDTH, WINDOW_HEIGHT, {
+		vsync = true,
+		fullscreen = false,
+		resizable = false
+	})
 end 
+
+function love.resize(w, h)
+	push:resizable(w, h)
+end
+
+function love.keyPressed(key)
+	if key == 'escape' then
+		love.event.quit()
+	end
+end
