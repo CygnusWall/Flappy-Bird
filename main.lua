3fad1ebcf2155ee146e0d6262bc0ae1ec8e06985
CygnusWall <96161984+CygnusWall@users.noreply.github.com>
2022-05-02 01:56:54 -0400

    Pipe spawning implemented

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ Class = require "class"
 --user defined classes
 require "Bird"
 
+require "Pipe"
+
 --constants
 WINDOW_WIDTH = 1366
 WINDOW_HEIGHT = 768
@@ -25,8 +27,15 @@ local GROUND_SCROLL_SPEED = 60
 
 local BACKGROUND_LOOPING_POINT = 569
 
+--seeds the random number generator
+math.randomseed(os.time())
+
 --making a bird object from the bird class
 local bird = Bird()
+
+local pipes = {}
+
+local timer = 0
 
 --is called when the game loads
 function love.load()
@@ -49,12 +58,15 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
+	--keeps track of when a key is pressed
 	love.keyboard.keysPressed[key] = true
+
 	if key == 'escape' then
 		love.event.quit()
 	end
 end
 
+--sorta stores the key that was pressed for later comparison, not totally sure
 function love.keyboard.wasPressed(key)
 	if love.keyboard.keysPressed[key] then
 		return true
@@ -71,8 +83,24 @@ function love.update(dt)
 	groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
 	% VIRTUAL_WIDTH
 
-	bird:update(dt)
+	timer = timer + dt
 
+	if timer > 2 then
+		table.insert(pipes, Pipe())
+		timer = 0
+	end
+
+	bird:update(dt)
+	
+	for k, pipe in pairs (pipes) do
+ 		pipe:update(dt)
+
+ 		if pipe.x < - pipe.width then
+ 			table.remove(pipes, k)
+ 		end
+	end
+
+	--clears the last stored key at the end of every frame so that a new one can be accepted
 	love.keyboard.keysPressed = {}
 end
 
@@ -80,6 +108,11 @@ function love.draw()
 	push:start()
 	--draws background and ground, they move because the x values are variables
 	love.graphics.draw(background, -backgroundScroll, 0)
+
+	for k, pipe in pairs(pipes) do
+		pipe:render()
+	end
+
 	love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
 	--user defined function for drawing the bird, see the class for more info
