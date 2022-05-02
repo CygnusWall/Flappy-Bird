715cfebc58b0096c24e0d54851d012fa1f9df82d
CygnusWall <96161984+CygnusWall@users.noreply.github.com>
2022-05-01 21:42:25 -0400

    Gravity works

--- Bird.lua
+++ Bird.lua
@@ -1,15 +1,31 @@
 Bird = Class{}
 
-function Bird:init{}
-	self.image = love.graphics.newImage(bird.png)
-	self.width = self.image:getWidth()
-	self.height = self.image.getHeight()
+--constants
+local GRAVITY = 10
 
+--is called when an object is created
+function Bird:init()
+	self.image = love.graphics.newImage('bird.png')
+	self.width = self.image:getWidth()
+	self.height = self.image:getHeight()
+
+	--sets the x and y positions to the center of the screen
 	self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
 	self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
 
+	self.dy = 0
 end
 
-function Bird:render{}
+function Bird:render()
+	--draws the bird at the center of the screen
 	love.graphics.draw(self.image, self.x, self.y)
 end
+
+function Bird:update(dt)
+	--increments gravity by itself every frame(scaled by delta time)
+	self.dy = self.dy + GRAVITY * dt
+
+	--increments the y pos of the bird every frame
+	self.y = self.y + self.dy
+end
+
--- main.lua
+++ main.lua
@@ -1,16 +1,19 @@
+--library required to use virtual resolution
 push = require "push"
-
+--library required to use classes
 Class = require "class"
 
+--user defined classes
 require "Bird"
 
-
-WINDOW_WIDTH = 1280
-WINDOW_HEIGHT = 720
+--constants
+WINDOW_WIDTH = 1366
+WINDOW_HEIGHT = 768
 
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
+--variables
 local background = love.graphics.newImage('background.png')
 local backgroundScroll = 0
 
@@ -20,20 +23,21 @@ local groundScroll = 0
 local BACKGROUND_SCROLL_SPEED = 30
 local GROUND_SCROLL_SPEED = 60
 
-local BACKGROUND_LOOPING_POINT = 570
+local BACKGROUND_LOOPING_POINT = 569
 
-
-
+--making a bird object from the bird class
 local bird = Bird()
 
+--is called when the game loads
 function love.load()
+	--disables interpolation giving us a crisp pixel art look
 	love.graphics.setDefaultFilter('nearest', 'nearest')
 
 	love.window.setTitle('Flappy Bird')
 
 	push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
 		vsync = true,
-		fullscreen = false,
+		fullscreen = true,
 		resizable = false
 	})
 end 
@@ -48,20 +52,24 @@ function love.keypressed(key)
 	end
 end
 
+--is called once every frame
 function love.update(dt)
 	backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt)
 	% BACKGROUND_LOOPING_POINT 
 
 	groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt)
 	% VIRTUAL_WIDTH
+
+	bird:update(dt)
 end
 
 function love.draw()
 	push:start()
-
+	--draws background and ground, they move because the x values are variables
 	love.graphics.draw(background, -backgroundScroll, 0)
 	love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
+	--user defined function for drawing the bird, see the class for more info
 	bird:render()
 
 	push:finish()
